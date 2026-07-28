57f9044b9e27f3dabdfeb232d9417476099d669b
Neel Vinod Bhansali <129235998+neel-bhan@users.noreply.github.com>
2026-07-27 21:59:28 -0400

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        c = Counter(tasks)
+        h = []
+        for k, v in c.items():
+            heapq.heappush(h, (-v, k))
+        r = deque()
+        time = 0
+        while h or r:
+            if not h:
+                time = r[0][0]
+            if r and r[0][0] <= time:
+                qt, qn,qv = r.popleft()
+                heapq.heappush(h,(qn, qv))
+            num, val = heapq.heappop(h)
+            time += 1
+            if -num-1 > 0:
+                r.append((time + n, num +1, val))    
+
+
+                
+
+                
+        return time
