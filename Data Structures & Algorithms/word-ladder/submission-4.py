9b0938e55c4be5f8e40ffd6d8526e45162f6ff2a
Neel Vinod Bhansali <129235998+neel-bhan@users.noreply.github.com>
2026-08-08 15:55:39 -0400

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.py
@@ -0,0 +1,58 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        d = defaultdict(list)
+        wordList.append(beginWord)
+
+        for w in wordList:
+            for i in range(len(w)):
+                d[w[:i] +"*" + w[i+1:]].append(w)
+        res = 0
+        seen = set()
+
+        qu = deque()
+        qu.append(beginWord)
+        seen = set()
+        while qu:
+            res += 1
+            for i in range(len(qu)):
+                word = qu.popleft()
+                
+                for i in range(len(word)):
+                    for nei in d[word[:i] +"*" + word[i+1:]]:
+                        if nei in seen:
+                            continue
+                        if nei == endWord:
+                            return res +1
+                        qu.append(nei)
+                        seen.add(nei) 
+                    
+        return 0
+
+
+
+        '''
+        def dfs(word, prev):
+            nonlocal res
+            if word == endWord:
+                return True
+            if word in seen:
+                return False
+            seen.add(word)
+            for i in range(len(word)):
+                for nei in d[word[:i] +"*" + word[i+1:]]:
+                    res += 1
+                    if nei == prev:
+                        continue
+                    if dfs(nei, word):
+                        print(word)
+                        return True
+                    res -=1
+        '''
+                
+        
+
+
+
+
+
+
